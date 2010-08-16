0dec0a42e3cef137ce72bb10175c179e6b48478c
Greg Turnquist <greg.turnquist@springsource.com>
2010-08-16 11:42:43 -0500

    Put in blocks of code so that developer can comment in/out parts to pick either pika or py-amqplib.

--- run_brokerage_standalone.py
+++ run_brokerage_standalone.py
@@ -1,7 +1,9 @@
-from amqplib_client import *
+#from amqplib_client import *
+from pika_client import *
 from buy_low_sell_high import *
 
-publisher = PyAmqpLibPublisher(exchange_name="my_exchange")
+#publisher = PyAmqpLibPublisher(exchange_name="my_exchange")
+publisher = PikaPublisher(exchange_name="my_exchange")
 buyer = Buyer(publisher, "", trend=25)
 print "Buyer = %s" % id(buyer)
 buyer.monitor()
--- run_ticker_standalone.py
+++ run_ticker_standalone.py
@@ -1,7 +1,9 @@
-from amqplib_client import *
+#from amqplib_client import *
+from pika_client import *
 from ticker_system import *
 
-publisher = PyAmqpLibPublisher(exchange_name="my_exchange")
+#publisher = PyAmqpLibPublisher(exchange_name="my_exchange")
+publisher = PikaPublisher(exchange_name="my_exchange")
 ticker = Ticker(publisher, "")
 ticker.monitor()
 
