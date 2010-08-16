91e8738fe271f23b393bafa7274e916838f310d4
Greg Turnquist <greg.turnquist@springsource.com>
2010-08-16 11:45:24 -0500

    Added comments to running the ticker service, telling dev how to pick either py-amqplib or pika.

--- run_ticker_standalone.py
+++ run_ticker_standalone.py
@@ -1,9 +1,26 @@
-#from amqplib_client import *
-from pika_client import *
-from ticker_system import *
+########################################
+# To run this demo using py-amqplib, 
+# uncomment this block, and  comment out
+# the next block.
+########################################
 
+#from amqplib_client import *
 #publisher = PyAmqpLibPublisher(exchange_name="my_exchange")
+
+########################################
+# To run this demo using pika,
+# uncomment this block, and comment out
+# the previous block
+########################################
+
+from pika_client import *
 publisher = PikaPublisher(exchange_name="my_exchange")
+
+########################################
+# This part doesn't have to change
+########################################
+
+from ticker_system import *
 ticker = Ticker(publisher, "")
 ticker.monitor()
 
