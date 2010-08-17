760f49957763fa24ab499c35a7b91f2eaba2c824
Greg Turnquist <greg.turnquist@springsource.com>
2010-08-17 11:43:46 -0500

    Create random stock symbols.

--- ticker_system.py
+++ ticker_system.py
@@ -6,7 +6,12 @@ import time
 class Ticker(object):
     def __init__(self, publisher, qname):
         self.publisher = publisher
-        self.stock_symbols = ["VMW", "S2", "VNX", "GOOGLE"]
+
+        # This quickly creates four random stock symbols
+        chars = range(ord("A"), ord("Z")+1)
+        def random_letter(): return chr(random.choice(chars))
+        self.stock_symbols = [random_letter()+random_letter()+random_letter() for i in range(4)]
+
         self.last_quote = {}
         self.counter = 0
         self.time_format = "%a, %d %b %Y %H:%M:%S +0000"
